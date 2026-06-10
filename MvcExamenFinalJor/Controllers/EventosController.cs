bcf94caa08648b01b6b388b65311f43aea75b87f
Ju102 <jlordonez02@gmail.com>
2026-06-10 11:05:18 +0200

    Add project files.

---
+++ MvcExamenFinalJor/Controllers/EventosController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcExamenFinalJor.Models;
+using MvcExamenFinalJor.Services;
+using System.Threading.Tasks;
+
+namespace MvcExamenFinalJor.Controllers
+{
+    public class EventosController : Controller
+    {
+        private readonly ServiceEventos service;
+
+        public EventosController(ServiceEventos service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Evento> eventos = await this.service.GetEventosAsync();
+            return View(eventos);
+        }
+
+        public async Task<IActionResult> EventosCategoria(int? idcategoria)
+        {
+            List<Categoria> categorias = await this.service.GetCategoriasAsync();
+            ViewData["categorias"] = categorias;
+
+            if (!idcategoria.HasValue)
+            {
+                List<Evento> eventos = await this.service.GetEventosAsync();
+                return View(eventos);
+            }
+            else
+            {
+                List<Evento> eventos = await this.service.GetEventosByIdCategoria(idcategoria.Value);
+                return View(eventos);
+            }
+        }
+    }
+}
---
+++ MvcExamenFinalJor/Services/ServiceEventos.cs
@@ -0,0 +1,89 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using MvcExamenFinalJor.Models;
+using System.Threading.Tasks;
+
+namespace MvcExamenFinalJor.Services
+{
+    public class ServiceEventos
+    {
+        private readonly string ApiUrl;
+        private MediaTypeWithQualityHeaderValue header;
+
+        public ServiceEventos(IConfiguration config)
+        {
+            this.ApiUrl = config.GetValue<string>("ConnectionStrings:EventosApiUrl");
+            this.header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        public async Task<List<Evento>> GetEventosAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/eventos/eventos/";
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+
+                HttpResponseMessage response = await client.GetAsync(this.ApiUrl + request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    List<Evento> eventos = await response.Content.ReadAsAsync<List<Evento>>();
+                    return eventos;
+                }
+                else
+                {
+                    return null;
+                }
+
+            }
+        }
+
+        public async Task<List<Categoria>> GetCategoriasAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/eventos/categorias/";
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+
+                HttpResponseMessage response = await client.GetAsync(this.ApiUrl + request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    List<Categoria> categorias = await response.Content.ReadAsAsync<List<Categoria>>();
+                    return categorias;
+                }
+                else
+                {
+                    return null;
+                }
+
+            }
+        }
+
+        public async Task<List<Evento>> GetEventosByIdCategoria(int idCategoria)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = $"api/eventos/eventocategoria/{idCategoria}/";
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+
+                HttpResponseMessage response = await client.GetAsync(this.ApiUrl + request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    List<Evento> eventos = await response.Content.ReadAsAsync<List<Evento>>();
+                    return eventos;
+                }
+                else
+                {
+                    return null;
+                }
+
+            }
+        }
+    }
+}
